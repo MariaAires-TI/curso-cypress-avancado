2ab0616b6e2e78aa12d4d941222faf2d40a629ce
Maria Aparecida <cidinhalima_12@hotmail.com>
2022-12-14 11:23:59 -0300

    feat: Mocking the API, extra credit 1: types and hits enter

--- cypress/integration/hackerStories.spec.js
+++ cypress/integration/hackerStories.spec.js
@@ -62,76 +62,86 @@ describe('Hacker Stories', () => {
   })
 
   context('Mocking the API', () => {
-    beforeEach(() => {
-      cy.intercept(
-        'GET',
-        `**/search?query=${initialTerm}&page=0`,
-        { fixture: 'stories' }
-      ).as('getStories')
-
-      cy.visit('/')
-      cy.wait('@getStories') // aqui estou pedindo para ele esperar até que a requisição seja feita
-    })
+    context('Footer and list of stories', () => {
+      beforeEach(() => {
+        cy.intercept(
+          'GET',
+          `**/search?query=${initialTerm}&page=0`,
+          { fixture: 'stories' }
+        ).as('getStories')
   
-    it('shows the footer', () => {
-      cy.get('footer')
-        .should('be.visible')
-        .and('contain', 'Icons made by Freepik from www.flaticon.com')
-    })
-  
-    context('List of stories', () => {
-      // Since the API is external,
-      // I can't control what it will provide to the frontend,
-      // and so, how can I assert on the data?
-      // This is why this test is being skipped.
-      // TODO: Find a way to test it out.
-      it.skip('shows the right data for all rendered stories', () => {})
-  
-      it.only('shows one less story after dimissing the first one', () => {         
-        cy.get('.button-small')
-          .first()
-          .click()       
-        
-        cy.get('.item').should('have.length', 1)
+        cy.visit('/')
+        cy.wait('@getStories') // aqui estou pedindo para ele esperar até que a requisição seja feita
       })
-  
-      // Since the API is external,
-      // I can't control what it will provide to the frontend,
-      // and so, how can I test ordering?
-      // This is why these tests are being skipped.
-      // TODO: Find a way to test them out.
-      context.skip('Order by', () => {
-        it('orders by title', () => {})
-  
-        it('orders by author', () => {})
-  
-        it('orders by comments', () => {})
-  
-        it('orders by points', () => {})
+    
+      it('shows the footer', () => {
+        cy.get('footer')
+          .should('be.visible')
+          .and('contain', 'Icons made by Freepik from www.flaticon.com')
+      })
+    
+      context('List of stories', () => {
+        // Since the API is external,
+        // I can't control what it will provide to the frontend,
+        // and so, how can I assert on the data?
+        // This is why this test is being skipped.
+        // TODO: Find a way to test it out.
+        it.skip('shows the right data for all rendered stories', () => {})
+    
+        it.only('shows one less story after dimissing the first one', () => {         
+          cy.get('.button-small')
+            .first()
+            .click()       
+          
+          cy.get('.item').should('have.length', 1)
+        })
+    
+        // Since the API is external,
+        // I can't control what it will provide to the frontend,
+        // and so, how can I test ordering?
+        // This is why these tests are being skipped.
+        // TODO: Find a way to test them out.
+        context.skip('Order by', () => {
+          it('orders by title', () => {})
+    
+          it('orders by author', () => {})
+    
+          it('orders by comments', () => {})
+    
+          it('orders by points', () => {})
+        })
       })
     })
+    
   
     context('Search', () => {
       beforeEach(() => {
         cy.intercept(
           'GET',
-            `**/search?query=${newTerm}&page=0`
-        ).as('getNewTermStories')
+            `**/search?query=${initialTerm}&page=0`,
+            { fixture: 'empty'}
+        ).as('getEmptyStories') //Esse intercept traz uma lista vazia 
+
+        cy.intercept(
+          'GET',
+            `**/search?query=${newTerm}&page=0`,
+            { fixture: 'stories'}
+        ).as('getStories') //Esse intercept traz uma lista de stories
+
+        cy.visit('/')
+        cy.wait('@getEmptyStories')
   
         cy.get('#search')
           .clear()
       })
   
-      it('types and hits ENTER', () => {
+      it.only('types and hits ENTER', () => {
         cy.get('#search')
           .type(`${newTerm}{enter}`)
   
-        cy.wait('@getNewTermStories')
+        cy.wait('@getStories')
   
-        cy.get('.item').should('have.length', 20)
-        cy.get('.item')
-          .first()
-          .should('contain', newTerm)
+        cy.get('.item').should('have.length', 2)
         cy.get(`button:contains(${initialTerm})`)
           .should('be.visible')
       })
